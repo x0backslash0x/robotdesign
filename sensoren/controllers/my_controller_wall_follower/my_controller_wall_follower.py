26dbe5d4cf2b1ed4d6c99d6042479e66445698a3
x0backslash0x <paddy0524@gmail.com>
2025-02-23 15:45:12 +0100

    prj-sensoren1 handson9 code controller wijzigen zoals getoont

--- sensoren/controllers/my_controller_wall_follower/my_controller_wall_follower.py
+++ sensoren/controllers/my_controller_wall_follower/my_controller_wall_follower.py
@@ -1,32 +1,45 @@
 """my_controller_wall_follower controller."""
-
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
 
-# create the Robot instance.
-robot = Robot()
+def run_robot(robot):  
+    # Get the time step of the current world.
+    timestep = int(robot.getBasicTimeStep())
+    max_speed = 6.28
+    
+    # Enable motors
+    leftMotor = robot.getDevice('left wheel motor')
+    rightMotor = robot.getDevice('right wheel motor')
+    
+    leftMotor.setPosition(float('inf'))
+    leftMotor.setVelocity(0.0)
+    
+    rightMotor.setPosition(float('inf'))
+    rightMotor.setVelocity(0.0)
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+    # Enable proximity sensors
+    prox_sensors = []
+    for ind in range(8):
+        sensor_name = 'ps' + str(ind)
+        prox_sensors.append(robot.getDistanceSensor(sensor_name))
+        prox_sensors[ind].enable(timestep)
+    
+    # Main loop:
+    # - perform simulation steps until Webots is stopping the controller
+    while robot.step(timestep) != -1:
+        # Read the sensors:
+        for ind in range(8):
+            print("ind: {}, val: {}".format(ind, prox_sensors[ind].getValue()))))
+        # Enter here functions to read sensor data, like:
+        #  val = ds.getValue()
+    
+        # Process sensor data here.
+    
+        # Enter here functions to send actuator commands, like:
+        leftMotor.setVelocity(max_speed)
+        rightMotor.setVelocity(max_speed)
+       
+if __name__ == "__main__":
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
-
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
-
-# Enter here exit cleanup code.
+    #create the robot instance
+    my_robot = Robot()
+    run_robot(myr_robot)
